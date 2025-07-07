35fa1ae070dd4f4ffcb77e37f2efa37426458c6b
maria eduarda ramos <mariaeduardarribeirogmail.com>
2025-07-07 00:55:06 -0300

    Add method adicionarQuantidade

--- js/js-av2.js
+++ js/js-av2.js
@@ -10,5 +10,25 @@ class Calcado {
         this.quantidade = 0;       // Quantidade em estoque (atributo numérico)
     }
 
-
+    /**Cadastra todos os atributos do calçado
+    *   
+    */
+    cadastrar(codigo, modelo, cor, tamanho, quantidade) {
+        this.codigo = codigo;          // Atribui o código do calçado
+        this.modelo = modelo;          // Atribui o modelo/descrição
+        this.cor = cor;                // Atribui a cor do calçado
+        this.tamanho = tamanho;        // Atribui o tamanho numérico
+        this.quantidade = quantidade;  // Atribui a quantidade em estoque
+        this._salvar();              // Chama o método para salvar os dados
+    }
+    /**Adiciona qunatidade ao estoque */
+    adicionarQuantidade(qtde) {
+        qtde = Number(qtde); // Converte a quantidade para número
+        if (isNaN(qtde) || qtde <= 0) {
+            throw new Error('Quantidade inválida para adição.');
+        }
+        this.quantidade += qtde; // Adiciona a quantidade ao estoque
+        this._salvar(); // Chama o método para salvar os dados
+        return this.quantidade; // Retorna a nova quantidade
+    }
 }    
